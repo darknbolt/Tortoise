2911c3a535e90eada2fc6e971604e747fd978303
DarkNBolt <mgrombiz@gmail.com>
2025-06-03 22:28:43 +0200

    Updated .gitignore and Switched to DB File

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,9 +1,4 @@
 INSERT INTO
-    AppUser (username, email, password)
-VALUES
-    ('darknbolt', 'test@email.com', '{noop}1234');
-
-INSERT INTO
     UserRole(name)
 VALUES
     ('Explorer'),
@@ -11,9 +6,3 @@ VALUES
     ('Developer'),
     ('Admin'),
     ('Owner');
-
-
-INSERT INTO
-    User_UserRole (user_ID, user_Role_ID)
-VALUES
-    (1, 5);
